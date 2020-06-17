5b2258671cf56ea01721b5da73036dd27e2d0654
Jason Cox <jasoncox@mac.com>
2020-06-17 09:16:08 -0400

    Initial Commit

---
+++ Magic 8-ball/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Magic 8-ball
+//
+//  Created by Jason Cox on 6/17/20.
+//  Copyright © 2020 Jason Cox. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
